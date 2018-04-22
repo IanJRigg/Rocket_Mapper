303268816b77e7b8e4c069f0645020dfa4d27f7b
Ian <ianjrigg@gmail.com>
2018-04-21 20:41:41 -0700

    Add google maps support

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Rocket_Mapper' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Rocket_Mapper
+  pod 'GoogleMaps'
+  pod 'Alamofire'
+
+  target 'Rocket_MapperTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Rocket_MapperUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- Rocket_Mapper/AppDelegate.swift
+++ Rocket_Mapper/AppDelegate.swift
@@ -7,15 +7,20 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        guard let path = Bundle.main.path(forResource: "ApiKeys", ofType: "plist") else { fatalError() }
+        let keys = NSDictionary(contentsOfFile: path)!
+
+        GMSServices.provideAPIKey(keys["GoogleMaps"] as! String)
         return true
     }
 
